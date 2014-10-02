aee8bb7c748666ec8b2d8aaa22b5c2b1c07d6b0a
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-02 00:54:41 -0400

    Business bean added.

---
+++ src/me/thesis/beans/Business.java
@@ -0,0 +1,105 @@
+package me.thesis.beans;
+
+import java.util.ArrayList;
+
+public class Business {
+	private String type;
+	private String businessId;
+	private String name;
+	private ArrayList<String> neighborhoods;
+	private String fullAddress;
+	private String city;
+	private String state;
+	private long latitude;
+	private long longitude;
+	private float stars;
+	private int reviewCount;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBusinessId() {
+		return businessId;
+	}
+
+	public void setBusinessId(String businessId) {
+		this.businessId = businessId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<String> getNeighborhoods() {
+		return neighborhoods;
+	}
+
+	public void setNeighborhoods(ArrayList<String> neighborhoods) {
+		this.neighborhoods = neighborhoods;
+	}
+
+	public String getFullAddress() {
+		return fullAddress;
+	}
+
+	public void setFullAddress(String fullAddress) {
+		this.fullAddress = fullAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public long getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(long latitude) {
+		this.latitude = latitude;
+	}
+
+	public long getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(long longitude) {
+		this.longitude = longitude;
+	}
+
+	public float getStars() {
+		return stars;
+	}
+
+	public void setStars(float stars) {
+		this.stars = stars;
+	}
+
+	public int getReviewCount() {
+		return reviewCount;
+	}
+
+	public void setReviewCount(int reviewCount) {
+		this.reviewCount = reviewCount;
+	}
+}
