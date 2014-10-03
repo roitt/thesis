dbaf497eab3d7526b6e4870081df2839d6ced3f1
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-03 02:50:41 -0400

    Business and review class.

---
+++ src/me/thesis/preferencepairs/beans/BusinessAndReview.java
@@ -0,0 +1,23 @@
+package me.thesis.preferencepairs.beans;
+
+public class BusinessAndReview {
+	private Business business;
+	private Review review;
+
+	public Business getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(Business business) {
+		this.business = business;
+	}
+
+	public Review getReview() {
+		return review;
+	}
+
+	public void setReview(Review review) {
+		this.review = review;
+	}
+
+}
